a5d91f4535d134e51f83c2646890ca6606a2d877
Your Name <alessanrospedroso@outlook.com>
2024-11-21 13:41:41 -0300

    descontruindo objetos e arrays

---
+++ Javascript - Evoluindo com ES6+/script.js
@@ -0,0 +1,27 @@
+let pessoas = {
+    name: "Alessandro",
+    sobrenome: "Perdroso",
+    empresa:"Al Sistemas Web"
+}
+
+// console.log(pessoas);
+// console.log(pessoas.empresa)
+
+// Desconstruir objeto
+
+// const { name:nomePessoa, empresa } = pessoas;
+
+// console.log(nomePessoa);
+// console.log(empresa);
+
+// Desconstruir um Array
+
+// ================================================
+
+let nomes = ["Alessandro", "Taís", "Eduardo"]
+let [primeiroNome, segundoNome, terceiroNome] = nomes
+console.log(primeiroNome)
+
+// let { 0: alessandro, 1:tais } = nomes;
+// console.log(alessandro)
+// console.log(tais)
